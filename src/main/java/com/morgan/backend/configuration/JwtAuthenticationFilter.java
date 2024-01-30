98d62ba00d8decdf00075359c0636cb53cc81895
Morgan <morgan124.bucher@gmail.com>
2024-01-30 15:56:44 +0100

    delete todo

--- src/main/java/com/morgan/backend/configuration/JwtAuthenticationFilter.java
+++ src/main/java/com/morgan/backend/configuration/JwtAuthenticationFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUserEmail(jwt); //todo extract userEmail from jwt Token
+        userEmail = jwtService.extractUserEmail(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
             if (jwtService.isTokenValid(jwt, userDetails)) {
